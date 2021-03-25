421babb7635b4df4be65eacd92608a44a0c8a474
ldw <2478010194@qq.com>
2021-03-26 02:18:31 +0800

    基于最小生成树的旅行商问题

--- src/TSP.java
+++ src/TSP.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class TSP{
+public class TSP {
     public static void main(String[] args) throws IOException {
         Solution S = new Solution();
         S.Kruskal();
@@ -12,19 +12,21 @@ public class TSP{
     }
 }
 
-class Solution{
-    public static final double MAX_WEIGHT=9999;
-    public double[][]Matrix=null;
-    public int[] Degree=null;
-    public ArrayList<Edge> Edges=new ArrayList<Edge>();
-    public ArrayList<Edge> MST=new ArrayList<Edge>();
-    public ArrayList<Edge>Hamiltonian=new ArrayList<Edge>();
-    public Set<Integer>D1=new HashSet<Integer>();
-    public Set<Integer>D0=new HashSet<Integer>();
+class Solution {
+    public static final double MAX_WEIGHT = 9999;
+    public double[][] Matrix = null;
+    public int[] Degree = null;
+    public ArrayList<Edge> Edges = new ArrayList<Edge>();
+    public ArrayList<Edge> MST = new ArrayList<Edge>();
+    public ArrayList<Edge> Hamiltonian = new ArrayList<Edge>();
+    public Set<Integer> D1 = new HashSet<Integer>();
+    public Set<Integer> D0 = new HashSet<Integer>();
+
     //构造函数
     public Solution() throws IOException {
         Initialize("data/test1.txt");
     }
+
     //读取文件，初始化边集和距离矩阵
     public void Initialize(String filepath) throws IOException {
         //读取txt文件
@@ -34,27 +36,29 @@ class Solution{
         BufferedReader buffReader = new BufferedReader(reader);
         //将字符串按空字符分割成字符串数组
         String[] strTmp = buffReader.readLine().split("\\s+");
-        int nodeCount=Integer.parseInt(strTmp[0]);
-        int edgeCount=Integer.parseInt(strTmp[1]);
-        Degree=new int[nodeCount];
-        Matrix=new double[nodeCount][nodeCount];
-        for(int i=0;i<Matrix.length;++i){
-            Arrays.fill(Matrix[i],MAX_WEIGHT);
-            Matrix[i][i]=0;
+        int nodeCount = Integer.parseInt(strTmp[0]);
+        int edgeCount = Integer.parseInt(strTmp[1]);
+        Degree = new int[nodeCount];
+        Matrix = new double[nodeCount][nodeCount];
+        for (int i = 0; i < Matrix.length; ++i) {
+            Arrays.fill(Matrix[i], MAX_WEIGHT);
+            Matrix[i][i] = 0;
         }
+        //读边集
         String strLine;
-        while((strLine= buffReader.readLine())!=null) {
+        while ((strLine = buffReader.readLine()) != null) {
             String[] edgeStr = strLine.split("\\s+");
             int node1 = Integer.parseInt(edgeStr[0]);
             int node2 = Integer.parseInt(edgeStr[1]);
             double weight = Double.parseDouble(edgeStr[2]);
-            Edges.add(new Edge(node1,node2,weight));
-            Matrix[node1][node2]=Matrix[node2][node1]= weight;
+            Edges.add(new Edge(node1, node2, weight));
+            Matrix[node1][node2] = Matrix[node2][node1] = weight;
             ++Degree[node1];
             ++Degree[node2];
         }
         buffReader.close();
     }
+
     //Kruskal算法获取最小生成树
     public void Kruskal() {
         Arrays.fill(Degree, 0);
@@ -79,6 +83,7 @@ class Solution{
             minheap.poll();
         }
     }
+
     //将最小生成树减支至各个结点的度为0,1,2
     public void Cut() {
         for (int i = MST.size() - 1; i >= 0; --i) {
@@ -104,6 +109,7 @@ class Solution{
                 D1.add(i);
         }
     }
+
     //将各个连通分量连成环
     public void MakeRing() {
         //处理独立点
@@ -151,30 +157,34 @@ class Solution{
             }
         }
     }
+
     //返回最短代价
-    public double SumWeight(){
-        double sum=0;
-        for(Edge e:Hamiltonian){
-            sum+=e.weight;
+    public double SumWeight() {
+        double sum = 0;
+        for (Edge e : Hamiltonian) {
+            sum += e.weight;
         }
         return sum;
     }
+
     //打印度为0的集合
-    public void ShowD0(){
-        for(int num:D0){
+    public void ShowD0() {
+        for (int num : D0) {
             System.out.println(num);
         }
     }
+
     //打印度为1的结点
-    public void ShowD1(){
-        for(int num:D1){
+    public void ShowD1() {
+        for (int num : D1) {
             System.out.println(num);
         }
     }
+
     //打印距离矩阵
-    public void ShowMatrix(){
-        for(double[] row:Matrix){
-            for(double w:row) {
+    public void ShowMatrix() {
+        for (double[] row : Matrix) {
+            for (double w : row) {
                 if (Math.abs(w - MAX_WEIGHT) < 1e-2)
                     System.out.printf("%-10.2f   ", -1.0);
                 else
@@ -183,45 +193,51 @@ class Solution{
             System.out.println();
         }
     }
+
     //打印属于哈密顿的边（MakeRing之后）
-    public void ShowHamiltonian(){
-        for(Edge e:Hamiltonian){
-            System.out.println(e.node1+" 到 "+e.node2+" 的权重为 "+e.weight);
+    public void ShowHamiltonian() {
+        for (Edge e : Hamiltonian) {
+            System.out.println(e.node1 + " 到 " + e.node2 + " 的权重为 " + e.weight);
         }
     }
+
     //打印边集
-    public void ShowEdges(){
-        for(Edge e:Edges){
-            System.out.println(e.node1+" 到 "+e.node2+" 的权重为 "+e.weight);
+    public void ShowEdges() {
+        for (Edge e : Edges) {
+            System.out.println(e.node1 + " 到 " + e.node2 + " 的权重为 " + e.weight);
         }
     }
+
     //打印最小生成树
-    public void ShowMST(){
-        for(Edge e:MST){
-            System.out.println(e.node1+" 到 "+e.node2+" 的权重为 "+e.weight);
+    public void ShowMST() {
+        for (Edge e : MST) {
+            System.out.println(e.node1 + " 到 " + e.node2 + " 的权重为 " + e.weight);
         }
     }
+
     //打印当前各个结点的度
-    public void ShowDegree(){
-        for(int i=0;i<Degree.length;++i){
-            System.out.println(i+" 的度为: "+Degree[i]);
+    public void ShowDegree() {
+        for (int i = 0; i < Degree.length; ++i) {
+            System.out.println(i + " 的度为: " + Degree[i]);
         }
     }
 }
 
 
-class Edge{
-    public int node1,node2;
+class Edge {
+    public int node1, node2;
     public double weight;
-    public Edge(int n1,int n2,double w){
-        node1=n1;
-        node2=n2;
-        weight=w;
+
+    public Edge(int n1, int n2, double w) {
+        node1 = n1;
+        node2 = n2;
+        weight = w;
     }
+
     //边集构造函数
     public Edge(Edge e) {
-        node1=e.node1;
-        node2=e.node2;
-        weight=e.weight;
+        node1 = e.node1;
+        node2 = e.node2;
+        weight = e.weight;
     }
 }
--- src/Unset.java
+++ src/Unset.java
@@ -1,37 +1,41 @@
 import com.sun.xml.internal.bind.v2.schemagen.xmlschema.Union;
 
 import java.util.Arrays;
-public class Unset{
+
+public class Unset {
     int[] Parent;
-    public Unset(int n){
-        Parent=new int[n];
-        Arrays.fill(Parent,-1);
+
+    public Unset(int n) {
+        Parent = new int[n];
+        Arrays.fill(Parent, -1);
     }
-    public int Find(int i){
-        while(Parent[i]>=0){
-            i=Parent[i];
+
+    public int Find(int i) {
+        while (Parent[i] >= 0) {
+            i = Parent[i];
         }
         return i;
     }
-    public void Merge(int i,int j){
-        int pi=Find(i);
-        int pj=Find(j);
-        if(pi!=pj){
-            if(Parent[pi]<Parent[pj]){//pi子女多,pj并入pi
-                Parent[pi]+=Parent[pj];
-                Parent[pj]=pi;
-            }
-            else{
-                Parent[pj]+=Parent[pi];
-                Parent[pi]=pj;
+
+    public void Merge(int i, int j) {
+        int pi = Find(i);
+        int pj = Find(j);
+        if (pi != pj) {
+            if (Parent[pi] < Parent[pj]) {//pi子女多,pj并入pi
+                Parent[pi] += Parent[pj];
+                Parent[pj] = pi;
+            } else {
+                Parent[pj] += Parent[pi];
+                Parent[pi] = pj;
             }
         }
     }
-    public void Show(){
-        for(int i=0;i<Parent.length;++i){
-            int pi=Find(i);
-            if(i!=pi){
-                System.out.println(i+" 属于 "+pi+"(有"+-Parent[pi]+"个孩子)");
+
+    public void Show() {
+        for (int i = 0; i < Parent.length; ++i) {
+            int pi = Find(i);
+            if (i != pi) {
+                System.out.println(i + " 属于 " + pi + "(有" + -Parent[pi] + "个孩子)");
             }
         }
 
